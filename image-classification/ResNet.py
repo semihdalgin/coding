06384794fe0a5f2e31bd7c9c14a41fd080195c0f
Semih Dalgin <40957492+semihdalgin@users.noreply.github.com>
2022-07-28 18:23:13 +0200

    Image Classification

--- image-classification/ResNet.py
+++ image-classification/ResNet.py
@@ -2,7 +2,7 @@
 import torch
 import torchvision
 import torchvision.datasets as datasets
-from torch.utils.data import Dataset, DataLoader,random_split
+from torch.utils.data import Dataset, DataLoader, random_split
 import torchvision.transforms as transforms
 from torchvision.datasets.utils import download_url
 from torchvision.datasets import ImageFolder
@@ -12,61 +12,62 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data.sampler import SubsetRandomSampler
 
-
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import tarfile
 
-
 import matplotlib
+
 # %matplotlib inline
 
+
+DATA_PATH = "/Users/semihdalgin/Desktop/coding/coding/image-classification/images"
+
+print("DATA_PATH = ", DATA_PATH)
+
+
 def get_default_device():
- """Pick GPU if available, else CPU"""
- if torch.cuda.is_available():
-  return torch.device('cuda')
- else:
-  return torch.device('cpu')
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
 
 
 def to_device(data, device):
- """Move tensor(s) to chosen device"""
- if isinstance(data, (list, tuple)):
-  return [to_device(x, device) for x in data]
- return data.to(device, non_blocking=True)
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
 
 
 class DeviceDataLoader():
- """Wrap a dataloader to move datİa to a device"""
+    """Wrap a dataloader to move datİa to a device"""
 
- def __init__(self, dl, device):
-  self.dl = dl
-  self.device = device
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
 
- def __iter__(self):
-  """Yield a batch of data after moving it to device"""
-  for b in self.dl:
-   yield to_device(b, self.device)
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
 
- def __len__(self):
-  """Number of batches"""
-  return len(self.dl)
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
 
- device = get_default_device()
- device
+    device = get_default_device()
+    device
 
- DATA_PATH = "/Users/semihdalgin/Desktop/coding/coding/image-classification/images"
-
- print("DATA_PATH = ", DATA_PATH)
-
- # Data transforms (normalization & data augmentation)
- stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
- train_tfms = transforms.Compose([transforms.RandomCrop(64, padding=4, padding_mode='reflect'),
-                                  transforms.RandomHorizontalFlip(),
-                                  transforms.ToTensor(),
-                                  transforms.Normalize(*stats, inplace=True)])
- valid_tfms = transforms.Compose([transforms.ToTensor(), tt.Normalize(*stats)])
+    # Data transforms (normalization & data augmentation)
+    stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    train_tfms = transforms.Compose([transforms.RandomCrop(64, padding=4, padding_mode='reflect'),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize(*stats, inplace=True)])
+    valid_tfms = transforms.Compose([transforms.ToTensor(), transforms.Normalize(*stats)])
 
 
 train_transforms = transforms.Compose([transforms.Resize(64),
@@ -75,32 +76,31 @@ train_transforms = transforms.Compose([transforms.Resize(64),
 test_transforms = transforms.Compose([transforms.Resize(64),
                                       transforms.ToTensor(),
                                       ])
-train_data = datasets.ImageFolder(DATA_PATH,
-                    transform=train_transforms)
-test_data = datasets.ImageFolder(DATA_PATH,
-                    transform=test_transforms)
+train_data = datasets.ImageFolder(DATA_PATH, transform=train_transforms)
+test_data = datasets.ImageFolder(DATA_PATH, transform=test_transforms)
 
 batch_size = 8
 
 # PyTorch data loaders
 train_dl = DataLoader(train_data, batch_size, shuffle=True, num_workers=2, pin_memory=True)
-valid_dl = DataLoader(test_data, batch_size*2, num_workers=2, pin_memory=True)
+valid_dl = DataLoader(test_data, batch_size * 2, num_workers=2, pin_memory=True)
 
 
 def denormalize(images, means, stds):
- means = torch.tensor(means).reshape(1, 3, 1, 1)
- stds = torch.tensor(stds).reshape(1, 3, 1, 1)
- return images * stds + means
+    means = torch.tensor(means).reshape(1, 3, 1, 1)
+    stds = torch.tensor(stds).reshape(1, 3, 1, 1)
+    return images * stds + means
 
 
 def show_batch(dl):
- for images, labels in dl:
-  fig, ax = plt.subplots(figsize=(12, 12))
-  ax.set_xticks([]);
-  ax.set_yticks([])
-  denorm_images = denormalize(images, *stats)
-  ax.imshow(make_grid(denorm_images[:64], nrow=8).permute(1, 2, 0).clamp(0, 1))
-  break
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 12))
+        ax.set_xticks([]);
+        ax.set_yticks([])
+        denorm_images = denormalize(images, *stats)
+        ax.imshow(make_grid(denorm_images[:64], nrow=8).permute(1, 2, 0).clamp(0, 1))
+        break
+
 
 show_batch(train_dl)
 
@@ -109,93 +109,93 @@ valid_dl = DeviceDataLoader(valid_dl, device)
 
 
 class SimpleResidualBlock(nn.Module):
- def __init__(self):
-  super().__init__()
-  self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
-  self.relu1 = nn.ReLU()
-  self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
-  self.relu2 = nn.ReLU()
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
 
- def forward(self, x):
-  out = self.conv1(x)
-  out = self.relu1(out)
-  out = self.conv2(out)
-  return self.relu2(out) + x  # ReLU can be applied before or after adding the input
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x  # ReLU can be applied before or after adding the input
 
- simple_resnet = to_device(SimpleResidualBlock(), device)
+    simple_resnet = to_device(SimpleResidualBlock(), device)
 
- for images, labels in train_dl:
-  out = simple_resnet(images)
-  print(out.shape)
-  break
+    for images, labels in train_dl:
+        out = simple_resnet(images)
+        print(out.shape)
+        break
 
- del simple_resnet, images, labels
- torch.cuda.empty_cache()
+    del simple_resnet, images, labels
+    torch.cuda.empty_cache()
 
- def accuracy(outputs, labels):
-  _, preds = torch.max(outputs, dim=1)
-  return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+    def accuracy(outputs, labels):
+        _, preds = torch.max(outputs, dim=1)
+        return torch.tensor(torch.sum(preds == labels).item() / len(preds))
 
 
 class ImageClassificationBase(nn.Module):
- def training_step(self, batch):
-  images, labels = batch
-  out = self(images)  # Generate predictions
-  loss = F.cross_entropy(out, labels)  # Calculate loss
-  return loss
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
 
- def validation_step(self, batch):
-  images, labels = batch
-  out = self(images)  # Generate predictions
-  loss = F.cross_entropy(out, labels)  # Calculate loss
-  acc = accuracy(out, labels)  # Calculate accuracy
-  return {'val_loss': loss.detach(), 'val_acc': acc}
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
 
- def validation_epoch_end(self, outputs):
-  batch_losses = [x['val_loss'] for x in outputs]
-  epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
-  batch_accs = [x['val_acc'] for x in outputs]
-  epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
-  return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
 
- def epoch_end(self, epoch, result):
-  print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-   epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
 
-  def conv_block(in_channels, out_channels, pool=False):
-   layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-             nn.BatchNorm2d(out_channels),
-             nn.ReLU(inplace=True)]
-   if pool: layers.append(nn.MaxPool2d(2))
-   return nn.Sequential(*layers)
+        def conv_block(in_channels, out_channels, pool=False):
+            layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                      nn.BatchNorm2d(out_channels),
+                      nn.ReLU(inplace=True)]
+            if pool: layers.append(nn.MaxPool2d(2))
+            return nn.Sequential(*layers)
 
 
 class ResNet9(ImageClassificationBase):
- def __init__(self, in_channels, num_classes):
-  super().__init__()
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
 
-  self.conv1 = conv_block(in_channels, 64)
-  self.conv2 = conv_block(64, 128, pool=True)
-  self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        self.conv1 = conv_block(in_channels, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
 
-  self.conv3 = conv_block(128, 256, pool=True)
-  self.conv4 = conv_block(256, 512, pool=True)
-  self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
 
-  self.classifier = nn.Sequential(nn.AdaptiveMaxPool2d(1),
-                                  nn.Flatten(),
-                                  nn.Dropout(0.2),
-                                  nn.Linear(512, num_classes))
+        self.classifier = nn.Sequential(nn.AdaptiveMaxPool2d(1),
+                                        nn.Flatten(),
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512, num_classes))
 
- def forward(self, xb):
-  out = self.conv1(xb)
-  out = self.conv2(out)
-  out = self.res1(out) + out
-  out = self.conv3(out)
-  out = self.conv4(out)
-  out = self.res2(out) + out
-  out = self.classifier(out)
-  return out
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
 
 
 model = to_device(ResNet9(3, 4), device)
@@ -204,55 +204,56 @@ model
 
 @torch.no_grad()
 def evaluate(model, val_loader):
- model.eval()
- outputs = [model.validation_step(batch) for batch in val_loader]
- return model.validation_epoch_end(outputs)
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
 
 
 def get_lr(optimizer):
- for param_group in optimizer.param_groups:
-  return param_group['lr']
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
 
 
 def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader,
                   weight_decay=0, grad_clip=None, opt_func=torch.optim.SGD):
- torch.cuda.empty_cache()
- history = []
+    torch.cuda.empty_cache()
+    history = []
 
- # Set up custom optimizer with weight decay
- optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay)
- # Set up one-cycle learning rate scheduler
- sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs,
-                                             steps_per_epoch=len(train_loader))
+    # Set up custom optimizer with weight decay
+    optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay)
+    # Set up one-cycle learning rate scheduler
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs,
+                                                steps_per_epoch=len(train_loader))
 
- for epoch in range(epochs):
-  # Training Phase
-  model.train()
-  train_losses = []
-  lrs = []
-  for batch in train_loader:
-   loss = model.training_step(batch)
-   train_losses.append(loss)
-   loss.backward()
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        lrs = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
 
-   # Gradient clipping
-   if grad_clip:
-    nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+            # Gradient clipping
+            if grad_clip:
+                nn.utils.clip_grad_value_(model.parameters(), grad_clip)
 
-   optimizer.step()
-   optimizer.zero_grad()
+            optimizer.step()
+            optimizer.zero_grad()
 
-   # Record & update learning rate
-   lrs.append(get_lr(optimizer))
-   sched.step()
+            # Record & update learning rate
+            lrs.append(get_lr(optimizer))
+            sched.step()
 
-  # Validation phase
-  result = evaluate(model, val_loader)
-  result['train_loss'] = torch.stack(train_losses).mean().item()
-  result['lrs'] = lrs
-  model.epoch_end(epoch, result)
-  history.append(result)
- return history
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
 
 epochs = 10
 max_lr = 0.01
@@ -261,54 +262,56 @@ weight_decay = 1e-4
 opt_func = torch.optim.Adam
 
 history = fit_one_cycle(epochs, max_lr, model, train_dl, valid_dl,
-                             grad_clip=grad_clip,
-                             weight_decay=weight_decay,
-                             opt_func=opt_func)
+                        grad_clip=grad_clip,
+                        weight_decay=weight_decay,
+                        opt_func=opt_func)
 
 history = [evaluate(model, valid_dl)]
 history
 
 
 def plot_accuracies(history):
- accuracies = [x['val_acc'] for x in history]
- plt.plot(accuracies, '-x')
- plt.xlabel('epoch')
- plt.ylabel('accuracy')
- plt.title('Accuracy vs. No. of epochs');
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs');
 
- plot_accuracies(history)
+    plot_accuracies(history)
 
- def plot_losses(history):
-  train_losses = [x.get('train_loss') for x in history]
-  val_losses = [x['val_loss'] for x in history]
-  plt.plot(train_losses, '-bx')
-  plt.plot(val_losses, '-rx')
-  plt.xlabel('epoch')
-  plt.ylabel('loss')
-  plt.legend(['Training', 'Validation'])
-  plt.title('Loss vs. No. of epochs');
+    def plot_losses(history):
+        train_losses = [x.get('train_loss') for x in history]
+        val_losses = [x['val_loss'] for x in history]
+        plt.plot(train_losses, '-bx')
+        plt.plot(val_losses, '-rx')
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.legend(['Training', 'Validation'])
+        plt.title('Loss vs. No. of epochs');
 
- plot_losses(history)
+    plot_losses(history)
 
- def plot_lrs(history):
-  lrs = np.concatenate([x.get('lrs', []) for x in history])
-  plt.plot(lrs)
-  plt.xlabel('Batch no.')
-  plt.ylabel('Learning rate')
-  plt.title('Learning Rate vs. Batch no.');
+    def plot_lrs(history):
+        lrs = np.concatenate([x.get('lrs', []) for x in history])
+        plt.plot(lrs)
+        plt.xlabel('Batch no.')
+        plt.ylabel('Learning rate')
+        plt.title('Learning Rate vs. Batch no.');
+
 
 plot_lrs(history)
 
 
 def predict_image(img, model):
- # Convert to a batch of 1
- xb = to_device(img.unsqueeze(0), device)
- # Get predictions from model
- yb = model(xb)
- # Pick index with highest probability
- _, preds = torch.max(yb, dim=1)
- # Retrieve the class label
- return train_data.classes[preds[0].item()]
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    _, preds = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return train_data.classes[preds[0].item()]
+
 
 img, label = test_data[0]
 plt.imshow(img.permute(1, 2, 0).clamp(0, 1))
