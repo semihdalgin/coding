b5939ff93c703a7ce88408697db50c4a9c223c53
Semih Dalgin <40957492+semihdalgin@users.noreply.github.com>
2022-08-04 21:46:46 +0200

    Cryptography

--- cyrpto.py
+++ cyrpto.py
@@ -23,3 +23,27 @@ message = "dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi r
 keyword = "friends"
 
 print(vigenere_decoder(message, keyword))
+
+def vigenere_coder(message, keyword):
+    letter_pointer = 0
+    keyword_final = ''
+    for i in range(0,len(message)):
+        if message[i] in punctuation:
+            keyword_final += message[i]
+        else:
+            keyword_final += keyword[letter_pointer]
+            letter_pointer = (letter_pointer+1)%len(keyword)
+    translated_message = ''
+    for i in range(0,len(message)):
+        if message[i] not in punctuation:
+            ln = alphabet.find(message[i]) + alphabet.find(keyword_final[i])
+            translated_message += alphabet[ln % 26]
+        else:
+            translated_message += message[i]
+    return translated_message
+
+message_for_v = "thanks for teaching me all these cool ciphers! you really are the best!"
+keyword = "semihdalgin"
+
+print(vigenere_coder(message_for_v,keyword))
+print(vigenere_decoder(vigenere_coder(message_for_v, keyword), keyword))
