db5447a7f281cdc6cb738335a82128d61fc3de62
Semih Dalgin <40957492+semihdalgin@users.noreply.github.com>
2022-08-17 15:58:27 +0200

    QUANTITATIVE AND CATEGORICAL VARIABLES

--- deneme.py
+++ deneme.py
@@ -3,3 +3,12 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+check_if_A_grade= lambda grade: "Got an A!" if grade >= 90 else "Did not get an A..."
+
+
